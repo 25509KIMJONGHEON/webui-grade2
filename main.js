57f68e270b609963ff44f9238e18ca39900e84db
“25509”KIMJONGHEON” <25509@occ.ac.jp>
2026-04-23 18:00:48 +0900

    +ボタンとーボタンを作った

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-// const name = prompt("名前は？");
-// alert("こんにちは、" + name + "さん！");
+function sayHello() {
+    alert("Hello");
+}
 
-// const target = "<h1 id='target'>Hello world</h1>";
-const target = document.getElementById("target");
-target.textContent = "こんにちは!";
-target.style.color = "red";
-target.style.fontSize = "10px";
-target.style.backgroundColor = "yellow";
+functuon FUNCTTON_NAME() {
+    //YOUR FUNCTION
